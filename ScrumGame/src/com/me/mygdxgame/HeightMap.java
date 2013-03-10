45c635b3de0469f7931b9a84684287e5a70520a2
BenjaminSergent <BenjaminRSergent@gmail.com>
2013-03-10 00:23:09 -0800

    Inital Commit. Is currently quick prototype of map creation

---
+++ ScrumGame/src/com/me/mygdxgame/HeightMap.java
@@ -0,0 +1,17 @@
+package com.me.mygdxgame;
+
+import jLibNoise.noise.module.Perlin;
+
+public class HeightMap {
+	public float[][] values;
+	public HeightMap(int sizeX, int sizeY) {
+		Perlin noiseMaker = new Perlin( );
+		values = new float[sizeX][sizeY];
+		
+		for(int x = 0; x < sizeX; x++) {
+			for(int y = 0; y < sizeY; y++) {
+				values[x][y] = (float) noiseMaker.GetValue(x/4.0, y/4.0, 1);
+			}
+		}
+	}
+}
